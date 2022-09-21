55838f57a750d2a21c6376b0162bbe3f0f0e1750
Hwiet <42421547+Hwiet@users.noreply.github.com>
2022-09-21 15:44:06 -0400

    Remove home

--- TaskApi/App_Start/RouteConfig.cs
+++ TaskApi/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TaskApi
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Quotes", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
