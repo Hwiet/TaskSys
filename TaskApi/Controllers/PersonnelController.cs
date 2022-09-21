cf507778c2e567b1aee01ae83451d4d7f90b8def
Hwiet <42421547+Hwiet@users.noreply.github.com>
2022-09-21 10:53:06 -0400

    Add controllers for all database entities

---
+++ TaskApi/Controllers/PersonnelController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TaskApi;
+
+namespace TaskApi.Controllers
+{
+    public class PersonnelController : ApiController
+    {
+        private TaskDBEntities db = new TaskDBEntities();
+
+        // GET: api/Personnel
+        public IQueryable<Personnel> GetPersonnels()
+        {
+            return db.Personnels;
+        }
+
+        // GET: api/Personnel/5
+        [ResponseType(typeof(Personnel))]
+        public IHttpActionResult GetPersonnel(int id)
+        {
+            Personnel personnel = db.Personnels.Find(id);
+            if (personnel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(personnel);
+        }
+
+        // PUT: api/Personnel/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPersonnel(int id, Personnel personnel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != personnel.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(personnel).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonnelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Personnel
+        [ResponseType(typeof(Personnel))]
+        public IHttpActionResult PostPersonnel(Personnel personnel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Personnels.Add(personnel);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (PersonnelExists(personnel.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = personnel.Id }, personnel);
+        }
+
+        // DELETE: api/Personnel/5
+        [ResponseType(typeof(Personnel))]
+        public IHttpActionResult DeletePersonnel(int id)
+        {
+            Personnel personnel = db.Personnels.Find(id);
+            if (personnel == null)
+            {
+                return NotFound();
+            }
+
+            db.Personnels.Remove(personnel);
+            db.SaveChanges();
+
+            return Ok(personnel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PersonnelExists(int id)
+        {
+            return db.Personnels.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ TaskApi/Controllers/QuoteTypesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TaskApi;
+
+namespace TaskApi.Controllers
+{
+    public class QuoteTypesController : ApiController
+    {
+        private TaskDBEntities db = new TaskDBEntities();
+
+        // GET: api/QuoteTypes
+        public IQueryable<QuoteType> GetQuoteTypes()
+        {
+            return db.QuoteTypes;
+        }
+
+        // GET: api/QuoteTypes/5
+        [ResponseType(typeof(QuoteType))]
+        public IHttpActionResult GetQuoteType(int id)
+        {
+            QuoteType quoteType = db.QuoteTypes.Find(id);
+            if (quoteType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(quoteType);
+        }
+
+        // PUT: api/QuoteTypes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutQuoteType(int id, QuoteType quoteType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != quoteType.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(quoteType).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuoteTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/QuoteTypes
+        [ResponseType(typeof(QuoteType))]
+        public IHttpActionResult PostQuoteType(QuoteType quoteType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.QuoteTypes.Add(quoteType);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (QuoteTypeExists(quoteType.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = quoteType.Id }, quoteType);
+        }
+
+        // DELETE: api/QuoteTypes/5
+        [ResponseType(typeof(QuoteType))]
+        public IHttpActionResult DeleteQuoteType(int id)
+        {
+            QuoteType quoteType = db.QuoteTypes.Find(id);
+            if (quoteType == null)
+            {
+                return NotFound();
+            }
+
+            db.QuoteTypes.Remove(quoteType);
+            db.SaveChanges();
+
+            return Ok(quoteType);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool QuoteTypeExists(int id)
+        {
+            return db.QuoteTypes.Count(e => e.Id == id) > 0;
+        }
+    }
+}
