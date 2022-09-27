992be9c3677cb06f8d55d98f34284bbc40d8a941
Hwiet <42421547+Hwiet@users.noreply.github.com>
2022-09-27 07:15:04 -0400

    Create metadata class

---
+++ TaskApiNew/Models/QuotePartial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaskApiNew.Models
+{
+    [MetadataType(typeof(QuoteMetadata))]
+    public partial class Quote
+    {
+    }
+
+    public class QuoteMetadata
+    {
+
+    }
+}
